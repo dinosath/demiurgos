25277161a8747c57e20a054a5d47560ac178f923
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-08-27 21:48:21 +0300

    add installation step

---
+++ src/generator.rs
@@ -0,0 +1,237 @@
+use std::{fs, io, path};
+use std::error::Error;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
+use flate2::bufread::GzDecoder;
+use git2::Repository;
+use log::error;
+use reqwest::{get, Client};
+use serde::{Deserialize, Serialize};
+use tar::Archive;
+use tempfile::tempdir;
+use tokio::fs::{copy, create_dir_all, File};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tracing::{debug, info};
+use zip::ZipArchive;
+
+#[derive(Serialize, Deserialize)]
+pub struct Generator {
+    #[serde(rename = "apiVersion")]
+    api_version: String,
+
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "version")]
+    version: String,
+
+    #[serde(rename = "description")]
+    description: String,
+
+    #[serde(rename = "keywords")]
+    keywords: Vec<String>,
+
+    #[serde(rename = "home")]
+    home: String,
+
+    #[serde(rename = "sources")]
+    sources: Vec<String>,
+
+    #[serde(rename = "dependencies")]
+    dependencies: Vec<Dependency>,
+
+    #[serde(rename = "maintainers")]
+    maintainers: Vec<Maintainer>,
+
+    #[serde(rename = "icon")]
+    icon: String,
+
+    #[serde(rename = "deprecated")]
+    deprecated: String,
+
+    #[serde(rename = "annotations")]
+    annotations: Annotations,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Annotations {
+    #[serde(rename = "example")]
+    example: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Dependency {
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "version")]
+    version: String,
+
+    #[serde(rename = "repository")]
+    repository: String,
+
+    #[serde(rename = "condition")]
+    condition: String,
+
+    #[serde(rename = "tags")]
+    tags: Vec<String>,
+
+    #[serde(rename = "import-values")]
+    import_values: Vec<String>,
+
+    #[serde(rename = "alias")]
+    alias: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Maintainer {
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "email")]
+    email: String,
+
+    #[serde(rename = "url")]
+    url: String,
+}
+
+
+pub async fn install_template(uri: &String, destination: &PathBuf) {
+    let source = uri;
+    info!("Starting the install process...");
+    debug!("Source: {}, Destination: {}", source, destination.display());
+    let generator_dir = prepare_generator_source(uri).await.unwrap();
+    debug!("generator_dir:{}", generator_dir.display());
+    move_to_repo_root(generator_dir, destination).await.unwrap();
+}
+
+async fn validate_generator(generator_dir_path: PathBuf) {
+    debug!("Starting validation of {}",generator_dir_path.to_str().unwrap());
+    debug!("TODO!");
+    todo!()
+}
+
+async fn prepare_generator_source(uri: &str) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let path = Path::new(uri);
+    if path.is_dir() {
+        debug!("Uri is local directory: {:?}", path.display());
+        Ok(Path::new(uri).to_path_buf())
+    } else {
+        let temp_dir = tempdir().unwrap().into_path();
+        debug!("Created temporary directory: {:?}", temp_dir);
+        if uri.starts_with("https://github.com") {
+            if uri.ends_with(".zip") || uri.ends_with(".tar.gz") {
+                info!("Detected GitHub directory URL that is not a repo, downloading specific directory...");
+                download_and_extract(uri, &temp_dir);
+            } else {
+                info!("Detected GitHub directory URL that is a repo, cloning repo...");
+                clone_git_repo(uri, &temp_dir)?;
+            }
+            Ok(temp_dir)
+        } else if uri.ends_with(".zip") || uri.ends_with(".tar.gz") {
+            info!("Detected URL, downloading file...");
+            download_and_extract(uri, &temp_dir);
+            Ok(temp_dir)
+        } else {
+            return Err("Unsupported URI format".into());
+        }
+    }
+}
+
+/// Downloads and extracts an archive (ZIP or TAR.GZ) from a URL.
+async fn download_and_extract(uri: &str, extract_to: &Path) -> Result<(), Box<dyn std::error::Error>> {
+
+    let response= reqwest::get(uri).await?;
+
+    let file_path = extract_to.join("download.zip");
+
+    let mut file = File::create(&file_path).await.unwrap();
+    let content =  response.text().await.unwrap();
+    file.write_all(content.as_bytes());
+    let file = fs::File::open(file_path).unwrap();
+
+    if uri.ends_with(".zip") {
+        let mut zip = ZipArchive::new(file).unwrap();
+        zip.extract(extract_to)?;
+    } else if uri.ends_with(".tar.gz") {
+        let buffered_file = BufReader::new(file);
+        let tar_gz = GzDecoder::new(buffered_file);
+        let mut archive = Archive::new(tar_gz);
+        archive.unpack(extract_to)?;
+    } else {
+        return Err("Unsupported archive format".into());
+    }
+
+    Ok(())
+}
+
+/// Clones a Git repository to a temporary directory.
+fn clone_git_repo(repo_url: &str, clone_to: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    Repository::clone(repo_url, clone_to)?;
+    Ok(())
+}
+
+/// Copies a local file or folder to the temporary directory.
+fn copy_local_path(src: &str, dest: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let src_path = Path::new(src);
+
+    if src_path.is_dir() {
+        // Recursively copy the directory
+        fs::create_dir_all(dest)?;
+        for entry in fs::read_dir(src_path)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+            let dest_path = dest.join(entry.file_name());
+
+            if entry_path.is_dir() {
+                fs::create_dir_all(&dest_path)?;
+                copy_local_path(entry_path.to_str().unwrap(), &dest_path)?;
+            } else {
+                fs::copy(&entry_path, &dest_path)?;
+            }
+        }
+    } else if src_path.is_file() {
+        // Copy the file
+        fs::copy(src_path, dest.join(src_path.file_name().unwrap()))?;
+    } else {
+        return Err("Invalid source path".into());
+    }
+
+    Ok(())
+}
+
+/// Moves the generator folder to the repository root after validation.
+async fn move_to_repo_root(temp_dir: PathBuf, repo_root: &PathBuf) -> Result<(), io::Error> {
+    let path = temp_dir.join("Generator.yaml");
+    debug!("Path: {}", path.display());
+    let mut file = File::open(path).await.unwrap();
+
+    // Read the file contents asynchronously into a String
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await?;
+
+    // Deserialize from the string contents
+    let generator: Generator = serde_yaml::from_str(&contents)
+        .map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidData, "Failed to deserialize Generator.yaml"))?;
+
+    let generator_dir = Path::new(repo_root).join(generator.name.clone()).join(generator.version.clone());
+
+    info!("Installing generator {}:{} to directory {}",generator.name.clone(),generator.version.clone(),generator_dir.display());
+    if !generator_dir.exists() {
+        create_dir_all(&generator_dir);
+    }
+
+    // Copy the contents from the temp dir to the repo directory
+    for entry in fs::read_dir(temp_dir)? {
+        let entry = entry?;
+        let dest_path = generator_dir.join(entry.file_name());
+        if entry.path().is_dir() {
+            create_dir_all(&dest_path);
+            copy_local_path(entry.path().to_str().unwrap(), &dest_path);
+        } else {
+            copy(entry.path(), dest_path);
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod generator;
+
+use generator::install_template;
 use std::fs;
 use std::fs::File;
 use std::io::copy;
@@ -5,9 +8,10 @@ use std::path::Path;
 
 use clap::Parser;
 use clap_derive::Subcommand;
-use gix::Repository;
 use reqwest::Url;
 use semver::Version;
+use serde::{Deserialize, Serialize};
+use tokio::io::AsyncBufReadExt;
 use tracing::{debug, error, info};
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::format;
@@ -22,6 +26,21 @@ struct Cli {
     command: Commands,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Template {
+    name: String,
+    version: String,
+    description: String,
+    dependencies: Vec<Dependency>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Dependency {
+    name: String,
+    version: String,
+    repository: String,
+}
+
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// install template to local repo
@@ -34,6 +53,10 @@ enum Commands {
         /// the name of the new template
         name: String,
     },
+    Generate {
+        config: String,
+        uri: String,
+    }
 }
 
 #[tokio::main]
@@ -42,146 +65,67 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
+
     let cli = Cli::parse();
-    let dir = dirs::data_local_dir().unwrap().join("demiurgos");
-    info!("directory for installing templates: {:?}!", dir);
+    let local_repo = dirs::data_local_dir().unwrap().join("demiurgos");
+    info!("directory for demiurgos config and data: {:?}!", local_repo);
+    let local_repo_generators = local_repo.join("generators");
+    info!("directory for installing templates: {:?}!", local_repo_generators);
     match &cli.command {
         Commands::Install { url } => {
-            info!("dir to install templates: {:?}!", url);
-            let source = url;
-            let binding = dirs::data_local_dir().unwrap().join("demiurgos");
-            let destination = binding.to_str().unwrap();
-
-            info!("Starting the install process...");
-            debug!("Source: {}, Destination: {}", source, destination);
-
-            if source.starts_with("https://github.com") && source.contains("/tree/") {
-                info!("Detected GitHub directory URL that is not a repo, downloading specific directory...");
-                download_github_directory(source, destination).await;
-            } else if source.starts_with("http://") || source.starts_with("https://") {
-                info!("Detected URL, downloading file...");
-                download_from_url(source, destination).await;
-            } else if source.ends_with(".git") {
-                info!("Detected Git repository URL, cloning repository...");
-                clone_git_repo(source, destination);
-            } else {
-                info!("Detected local file or directory path, copying...");
-                copy_from_path(source, destination);
-            }
-            info!("Installation process completed.");
+            info!("dir to install templates: {:?}!", local_repo_generators);
+            install_template(&url, &local_repo_generators).await;
         },
         Commands::New { name } => {
             info!("Creating new template: {name}");
-        }
+            create_new_template(name);
+        },
+        Commands::Generate { config, uri } => {
+            todo!()
+        },
+
     }
 }
 
-fn copy_from_path(source: &str, destination: &str) {
-    let source_path = Path::new(source);
-    let destination_path = Path::new(destination);
+/// Function to create the new template package
+fn create_new_template(name: &str) {
+    // Define the directory structure and file contents
+    let package_dir = format!("./{}", name);
 
-    if source_path.is_file() {
-        debug!("Copying file from {} to {}", source, destination);
-        fs::copy(source_path, destination_path).expect("Failed to copy file");
-    } else if source_path.is_dir() {
-        debug!("Copying directory from {} to {}", source, destination);
-        fs::create_dir_all(destination_path).expect("Failed to create destination directory");
-        for entry in fs::read_dir(source_path).expect("Failed to read source directory") {
-            let entry = entry.expect("Failed to get directory entry");
-            let entry_path = entry.path();
-            let entry_destination = destination_path.join(entry.file_name());
-            copy_from_path(entry_path.to_str().unwrap(), entry_destination.to_str().unwrap());
-        }
-    } else {
-        info!("Source path does not exist");
-    }
-}
-
-fn get_existing_versions(repo_path: &Path) -> Vec<Version> {
-    let mut versions = Vec::new();
-
-    if let Ok(entries) = fs::read_dir(repo_path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if let Ok(file_name) = entry.file_name().into_string() {
-                    if let Ok(version) = Version::parse(&file_name) {
-                        versions.push(version);
-                    }
-                }
-            }
-        }
-    }
-    versions
-}
-
-async fn download_from_url(source: &str, destination: &str) {
-    let url = Url::parse(source).expect("Invalid URL");
-    let response = reqwest::get(url).await.expect("Failed to download file");
-    let mut file = File::create(destination).expect("Failed to create destination file");
-    let content = response.bytes().await.expect("Failed to read content");
-    copy(&mut content.as_ref(), &mut file).expect("Failed to write content");
-}
-
-async fn download_github_directory(source: &str, destination: &str) {
-    let (repo_url, path_in_repo) = extract_github_info(source);
-    let path= format!("repo_temp-{}",Uuid::new_v4());
-    let temp_dir = std::env::temp_dir().join(path);
-    let temp_zip_path = temp_dir.join("repo.zip");
-
-    fs::create_dir_all(&temp_dir).expect("Failed to create temp directory");
-
-    let archive_url = format!("{}/archive/refs/heads/master.zip", repo_url);
-    debug!("Downloading GitHub archive from {}", archive_url);
-    let response = reqwest::get(&archive_url).await.expect("Failed to download archive");
-    let mut file = File::create(&temp_zip_path).expect("Failed to create temp zip file");
-    let content = response.bytes().await.expect("Failed to read content");
-
-    debug!("Extracting from zip archive {:?}", temp_zip_path);
-    copy(&mut content.as_ref(), &mut file).expect("Failed to write to temp zip file");
-    let mut zip_file = File::open(&temp_zip_path).expect("Failed to open temp zip file");
-    let mut archive = ZipArchive::new(zip_file).expect("Failed to read zip archive");
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).expect("Failed to access file in zip");
-        let outpath = temp_dir.join(file.sanitized_name());
-
-        if (&*file.name()).ends_with('/') {
-            fs::create_dir_all(&outpath).expect("Failed to create directory");
-        } else {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(p).expect("Failed to create directory");
-                }
-            }
-            let mut outfile = File::create(&outpath).expect("Failed to create file");
-            copy(&mut file, &mut outfile).expect("Failed to copy file");
-        }
+    if Path::new(&package_dir).exists() {
+        error!("Directory '{}' already exists!", name);
+        return;
     }
 
-    // Move the specific directory to the destination
-    let specific_dir = temp_dir.join(format!("{}-master", repo_url.split('/').last().unwrap())).join(path_in_repo);
+    // Metadata file content
+    let metadata = Template {
+        name: name.to_string(),
+        version: "0.0.1".to_string(),
+        description: "A template for".to_string(),
+        dependencies: vec![],
+    };
 
-    if specific_dir.exists() && specific_dir.is_dir() {
-        debug!("Copying extracted directory to destination");
-        copy_from_path(specific_dir.to_str().unwrap(), destination);
-    } else {
-        error!("The specified directory does not exist in the repository");
-    }
+    let metadata_yaml = serde_yaml::to_string(&metadata).unwrap();
+    serde_yaml::to_writer(File::create(&format!("{}/template.yaml", package_dir)).unwrap(), &metadata_yaml).unwrap();
+    fs::create_dir_all(&package_dir).unwrap();
 
-    // Clean up
-    fs::remove_dir_all(temp_dir).expect("Failed to remove temp directory");
+    // Create the files
+    create_file(&format!("{}/template.yaml", package_dir), &metadata_yaml);
+    create_file(
+        &format!("{}/README.md", package_dir),
+        "# Template README\n\nThis is your template's README file.",
+    );
+    create_file(
+        &format!("{}/template.html", package_dir),
+        "<!-- Your template content here -->",
+    );
+
+    println!("Template package '{}' has been created!", name);
 }
 
-fn extract_github_info(github_url: &str) -> (String, String) {
-    let parts: Vec<&str> = github_url.split("/tree/master/").collect();
-    let repo_url = parts[0].replace("https://github.com", "https://github.com");
-    let path_in_repo = if parts.len() == 2 { parts[1] } else { "" };
-    (repo_url, path_in_repo.to_string())
-}
-
-
-fn clone_git_repo(source: &str, destination: &str) {
-    //TODO
-    // clone git repo to destination
-    return;
+/// Helper function to create a file with content
+fn create_file(path: &str, content: &str) {
+    let mut file = File::create(path).unwrap();
+    // file.write_all(content.as_bytes()).unwrap();
+    println!("Created file: {}", path);
 }
